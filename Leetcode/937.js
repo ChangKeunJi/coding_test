7b806386ffc2e4036f67ff00ecafc09c8375b3d7
changkeunji <ckdrmsdk@gmail.com>
2022-01-20 21:16:11 +0900

    Fail | 로그 재정렬

--- Leetcode/937.js
+++ Leetcode/937.js
@@ -6,63 +6,30 @@ const logs = [
 	"let3 art zero",
 ];
 
-// let이 digit보다 먼저온다
-// letter log는 사전순으로 정렬되어있다
-// 문자가 동일한 경우 식별자 순으로 한다
-// 숫자 로그는 입력 순서를 유지한다
+const reorderLogFiles = (logs) => {
+	// 식별자 뒤에오는 단어들만 반환한다.
+	const body = (s) => s.slice(s.indexOf(" ") + 1);
 
-// 1. 새로운 배열을 만들고 입력받은 배열을 반복문으로 돌린다
-// 2. letter 로그를 발견하면 새로운 배열에 push한다
-// 3. letter 로그로 이루어진 배열을 다시 반복문으로 돌린다
-// 4. sort 메서드를 이용해 alphabetic 순서로 정렬한다
-// 5. 정렬한 배열 뒤에 남은 digit 원소들을 같다 붙인다
+	// number & string 타입 상관없이 숫자이면 true를 반환한다.
+	const isNum = (c) => /\d/.test(c);
 
-var reorderLogFiles = function (logs) {
-	// 1. 새로운 배열을 만들고 입력받은 배열을 반복문으로 돌린다
-	// 2. letter 로그를 발견하면 새로운 배열에 push한다
-	let digits = [];
-	let newArr = [];
+	// if body same then compare identifier
+	const compare = (a, b) => {
+		const n = body(a).localeCompare(body(b));
+		if (n !== 0) return n;
+		return a.localeCompare(b);
+	};
 
-	for (let el of logs) {
-		if (isNaN(Number(el[el.length - 1]))) {
-			newArr.push(el);
-		} else {
-			digits.push(el);
-		}
+	const digitLogs = [];
+	const letterLogs = [];
+	for (const log of logs) {
+		if (isNum(body(log))) digitLogs.push(log);
+		else letterLogs.push(log);
 	}
 
-	// 3. letter 로그로 이루어진 배열을 다시 반복문으로 돌린다
-	// 요소들의 단어 순서를 재정렬한다
-	// 4. sort 메서드를 이용해 alphabetic 순서로 정렬한다
+	console.log([...letterLogs.sort(compare), ...digitLogs]);
 
-	// let newArr2 = [];
-
-	// for (let el of newArr) {
-	// 	let tempArr = el.split(" ");
-	// 	let first = tempArr.splice(0, 1);
-	// 	tempArr.push(first[0]);
-	// 	el = tempArr;
-	// 	newArr2.push(el);
-	// }
-
-	// newArr2 = newArr2.sort();
-	// let newArr3 = [];
-
-	// for (let el of newArr2) {
-	// 	let last = el.splice(-1);
-	// 	newArr3.push([...last, ...el]);
-	// }
-
-	console.log(newArr.sort());
-
-	// 5. 정렬한 배열 뒤에 남은 digit 원소들을 같다 붙인다
-	let answer = [];
-
-	// for (let el of newArr3) {
-	// 	answer.push(el.join(" "));
-	// }
-	// // console.log([...answer, ...digits]);
-	// return [...answer, ...digits];
+	return [...letterLogs.sort(compare), ...digitLogs];
 };
 
 reorderLogFiles(logs);
