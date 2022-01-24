c30e5cb8541a9c99fb92bf84030affd00cad2386
changkeunji <ckdrmsdk@gmail.com>
2022-01-24 21:54:21 +0900

    문자열 | Group Anagram

--- Leetcode/49.js
+++ Leetcode/49.js
@@ -1,6 +1,18 @@
 const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-var groupAnagrams = function (strs) {};
+var groupAnagrams = function (strs) {
+	for (const el of strs) {
+		// el의 Map을 만든다.
+		const map = new Map();
 
+		for (const e of el) {
+			if (map.get(e)) {
+				map[e]++;
+			} else {
+				map.set(e, 1);
+			}
+		}
+	}
+};
 groupAnagrams(strs);
