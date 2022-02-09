c16cbed3762c741a5f5b95f08617456825d265af
changkeunji <ckdrmsdk@gmail.com>
2022-02-09 22:33:09 +0900

    Fail | 모든 경우의 수를 하나 씩 구해야한다

--- inflearn/section4/3.js
+++ inflearn/section4/3.js
@@ -1,5 +1,18 @@
 function foo(array) {
-	const arr = array.splice(0, 1);
+	let answer = 0;
+	for (let i = 1; i <= 4; i++) {
+		for (let j = 1; j <= 4; j++) {
+			let result = true;
+			for (const score of array) {
+				if (score.indexOf(i) <= score.indexOf(j)) result = false;
+			}
+			if (result) {
+				answer++;
+			}
+		}
+	}
+
+	return answer;
 }
 
 foo([
