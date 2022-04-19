ef39d1e4bc2fd722e62bf12b3229b2ec904b9f69
changkeun <ckdrmsdk@gmail.com>
2022-04-19 10:07:50 +0900

    자료구조 7

--- inflearn/section6/3.js
+++ inflearn/section6/3.js
@@ -1,4 +1,27 @@
-function solution(board, move) {}
+function solution(board, move) {
+	let stack = [];
+	let answer = 0;
+
+	for (let el of move) {
+		for (let i = 0; i < board.length; i++) {
+			if (board[i][el - 1] !== 0) {
+				// 연속된 인형일 때는 스택에서 마지막 인형 제거 후 answer에 2추가
+				if (stack[stack.length - 1] === board[i][el - 1]) {
+					answer += 2;
+					stack.pop();
+				} else {
+					// 연속된 인형이 아닐 때는 stack에 추가
+					stack.push(board[i][el - 1]);
+				}
+				board[i][el - 1] = 0;
+
+				break;
+			}
+		}
+	}
+
+	return answer;
+}
 
 let a = [
 	[0, 0, 0, 0, 0],
