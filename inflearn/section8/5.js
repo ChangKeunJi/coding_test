9c9ac519f7680b0df3cb1cda9d33b2f6b2f38b9a
changkeun <ckdrmsdk@gmail.com>
2022-04-22 20:55:51 +0900

    이진탐색 5

--- inflearn/section8/5.js
+++ inflearn/section8/5.js
@@ -1,4 +1,12 @@
-function solution(arr) {}
+function solution(arr) {
+	function solution(n) {
+		let arr = Array.from({ length: n + 1 }, () => 0);
+
+		DFS(1);
+
+		console.log(answer);
+	}
+}
 
 let arr = [1, 3, 5, 6, 7, 10];
 console.log(solution(arr));
