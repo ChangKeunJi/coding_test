001b2efea4c8db51db05244dd9a0b327789426c4
changkeun <ckdrmsdk@gmail.com>
2022-04-23 16:16:46 +0900

    이진탐색 6

--- inflearn/section8/4.js
+++ inflearn/section8/4.js
@@ -1,30 +1,33 @@
 function solution(n) {
-	let arr = Array.from({ length: n + 1 }, () => 0);
-
+	let array = Array.from({ length: n + 1 }, () => 0);
 	let answer = [];
 
 	function DFS(k) {
 		if (k === n + 1) {
 			let temp = [];
-			for (let i = 1; i < arr.length; i++) {
-				if (arr[i] === 1) {
+			console.log(array);
+
+			for (let i = 1; i < array.length; i++) {
+				if (array[i] === 1) {
 					temp.push(i);
 				}
 			}
 
 			if (temp.length > 0) answer.push(temp);
+
+			return;
 		} else {
-			arr[k] = 1;
+			array[k] = 1;
 			DFS(k + 1);
 
-			arr[k] = 0;
+			array[k] = 0;
 			DFS(k + 1);
 		}
 	}
 
 	DFS(1);
 
-	console.log(answer);
+	console.log("answer: ", answer);
 }
 
 solution(3);
