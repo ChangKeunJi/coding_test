4aaee895e54ceb357fa2358b5e06cf64ddbf2b9b
changkeun <ckdrmsdk@gmail.com>
2022-05-02 23:48:09 +0900

    프로그래머스 lv2 6

--- inflearn/section10/6.js
+++ inflearn/section10/6.js
@@ -1,123 +1,34 @@
-function solution(orders, course) {
-	let answer = [];
+function solution(cacheSize, cities) {
+	if (cacheSize === 0) return cities.length * 5;
+	if (cities.length === 0) return 0;
 
-	// 입력받은 orders 배열의 문자열 원소를 배열로 바꿔주고 알파벳 순으로 정렬한다.
-	let newOrders = orders.map((order) => {
-		return order.split("").sort().join("");
-	});
+	let cache = [];
+	let count = 0;
 
-	// 원소의 개수가 작은것 부터 먼저오게 다시 정렬한 뒤 배열원소를 다시 문자열로 변환한다.
-	newOrders = newOrders.sort((a, b) => a.length - b.length);
+	cities = cities.map((el) => el.toLowerCase());
 
-	// course를 순환하면서 갯수에 맞는 조합이 존재하는지 찾는다.
-	let commonArr = [];
+	while (cities.length) {
+		let city = cities.shift();
 
-	// 이중 for문 돌면서 공통 문자열 찾는다.
+		// 캐시에 있는지 확인한다.
+		let i = cache.findIndex((el) => el === city);
 
-	for (let i = 0; i < newOrders.length; i++) {
-		for (let j = i + 1; j < newOrders.length; j++) {
-			const [common, commonLeng] = findCommonStr(newOrders[i], newOrders[j]);
+		if (i >= 0) {
+			// 캐시에 저장된 경우 캐시 맨 뒤로 보낸다.
+			cache.splice(i, 1);
+			cache.push(city);
 
-			if (course.includes(commonLeng)) commonArr.push(common);
-		}
-	}
-
-	console.log("commonArr", commonArr);
-
-	// commonArr를 Map으로 만든다.
-	const map = {};
-
-	for (let el of commonArr) {
-		if (map[el]) {
-			map[el]++;
+			count += 1;
 		} else {
-			map[el] = 1;
-		}
-	}
-
-	console.log("map", map);
-
-	for (let el of course) {
-		let max = -1;
-		for (let prop in map) {
-			if (prop.length === el) {
-				max = Math.max(max, map[prop]);
+			// 저장되어 있지 않은 경우
+			if (cache.length < cacheSize) {
+				cache.push(city);
+			} else {
+				cache.shift();
+				cache.push(city);
 			}
-		}
-		console.log("max", max);
-		for (let prop in map) {
-			if (prop.length === el && map[prop] === max) {
-				answer.push(prop);
-			}
+			count += 5;
 		}
 	}
-
-	console.log(answer);
-	return answer.sort();
-}
-
-function findCommonStr(str1, str2) {
-	let common = "";
-
-	for (let el of str1) {
-		if (str2.includes(el)) common += el;
-	}
-
-	if (!common) return ["", 0];
-
-	return [common, common.length];
-}
-
-//2
-
-function solution(orders, course) {
-	let answer = [];
-
-	// 입력받은 orders 배열의 문자열 원소를 배열로 바꿔주고 알파벳 순으로 정렬한다.
-	let newOrders = orders.map((order) => {
-		return order.split("").sort().join("");
-	});
-
-	// 원소의 개수가 작은것 부터 먼저오게 다시 정렬한 뒤 배열원소를 다시 문자열로 변환한다.
-	newOrders = newOrders.sort((a, b) => a.length - b.length);
-	console.log(newOrders);
-
-	/* - newOrders 
-	[
-		'AB',    'CD',
-		'ADE',   'XYZ',
-		'XYZ',   'ACD',
-		'ABCDE'
-	]
-	*/
-
-	// course를 순환하면서 갯수에 맞는 조합이 존재하는지 찾는다.
-	let commonArr = [];
-
-	// 이중 for문 돌면서 공통 문자열 찾는다.
-	for (let i = 0; i < newOrders.length; i++) {
-		for (let j = i + 1; j < newOrders.length; j++) {
-			// const [common, commonLeng] = findCommonStr(newOrders[i], newOrders[j]);
-			console.log(findCommonStr(newOrders[i], newOrders[j], course));
-			// if(course.includes(commonLeng)) commonArr.push(common);
-		}
-	}
-}
-
-function findCommonStr(str1, str2, course) {
-	let arr = [];
-
-	// A, D, E / A, B, C, D, E
-	for (let c of course) {
-		// 2, 3, 5
-		let common = "";
-		for (let i = 0; i < str1.length; i++) {
-			if (str2.includes(str1[i])) {
-				common += str1[i];
-				if (common.length === c) {
-					arr.push(common);
-				}
-			}
-		}
-	}
+	return count;
 }
